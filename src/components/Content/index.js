a6c55b55e09f7c9a788cca93ee1b9cfc274ba256
Luan Junio Cicareli <luancicarelli3@gmail.com>
2023-12-11 22:09:20 -0300

    fix update

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -10,7 +10,12 @@ const Content = () => {
                 <Col>
                     <h3 style={{textAlign: "center"}}>Sobre mim</h3>
                     <p id="about" className="about">
-                        Sou Luan, apaixonado por programação. Desenvolvedor formado em Sistemas de Informação, com 5 anos de experiência na área de desenvolvimento Full Stack, e ainda conhecimentos e experiências em desenvolvimentos de projetos personalizado como painéis CMS integrado e CRM's, com conhecimento em Programação Orientada a Objetos e Padrão Design Patterns. Busco novas oportunidades para que possa aplicar e compartilhar meus conhecimentos.
+                        Sou Luan, apaixonado por programação. Desenvolvedor Full Stack com formação em Sistemas de Informação e mais de 6 anos de experiência.
+                        Especializado em projetos customizados, incluindo a implementação de painéis CMS integrados, sistemas CRM e EADs. Possuo conhecimentos em Programação Orientada a Objetos, Padrões de Design Patterns e integrações via APIs. 
+                    </p>
+                    <h3 style={{textAlign: "center"}}>Objetivo</h3>
+                    <p st className="about">
+                        Busco desafios, onde posso utilizar minha experiência em diversas linguagens e criar soluções impactantes. Comprometido em aplicar meus conhecimentos de forma eficiente, estou sempre aberto a aprender e evoluir. Procuro integrar uma equipe dinâmica, contribuindo para o sucesso da empresa enquanto continuo aprimorando minhas habilidades e conhecimentos técnicos. 
                     </p>
                 </Col>
             </Row>
