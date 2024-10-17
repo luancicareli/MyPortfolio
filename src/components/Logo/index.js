b21a72cbfec239ee2205f930fe175748dacf52b1
Luan Junio Cicareli <luancicarelli3@gmail.com>
2024-10-16 23:23:09 -0300

    fix url base

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -2,7 +2,7 @@ import './logo.css';
 
 const Logo = () => (
   <>
-    <div className="logo"><img alt="logo Luan" src="/images/logoLuan.png"/></div>
+    <div className="logo"><img alt="logo Luan" src={`${process.env.PUBLIC_URL}/images/logoLuan.png`} /></div>
   </>
 );
 export default Logo;
